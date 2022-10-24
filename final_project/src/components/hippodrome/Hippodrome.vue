07d13dbf7206ecdf9d4732fb41c338f30e309f96
Onur Çöpoğlu <onurcpgl.52@gmail.com>
2022-10-24 13:33:11 +0300

    Result Component added

--- final_project/src/components/hippodrome/Hippodrome.vue
+++ final_project/src/components/hippodrome/Hippodrome.vue
@@ -1,119 +1,125 @@
-<script setup>          
-import { ref,onMounted, onUpdated,watch} from "vue";
+<script setup>
+import { ref, watch,onMounted } from "vue";
+import Result from "../result/Result.vue";
 const props = defineProps(["gameStatusProp"]);
-
+const raceStatus = ref(false);
 const data = ref([
-    {
-        id:1,
-        name:"Devirhan",
-        speed:0,
-    },
-    {
-        id:2,
-        name:"Odin",
-        speed:500,
-    },
-    {
-        id:3,
-        name:"Sunday",
-        speed:0,
-    },
-    {
-        id:4,
-        name:"Timurhan",
-        speed:0,
-    },
-    {
-        id:5,
-        name:"Tansel",
-        speed:0,
-    },
-    {
-        id:6,
-        name:"Yelhan",
-        speed:0,
-    }
-])
-
-
+  {
+    id: 1,
+    name: "Devirhan",
+    speed: 0,
+  },
+  {
+    id: 2,
+    name: "Odin",
+    speed: 0,
+  },
+  {
+    id: 3,
+    name: "Sunday",
+    speed: 0,
+  },
+  {
+    id: 4,
+    name: "Timurhan",
+    speed: 0,
+  },
+  {
+    id: 5,
+    name: "Tansel",
+    speed: 0,
+  },
+  {
+    id: 6,
+    name: "Yelhan",
+    speed: 0,
+  },
+]);
 
 const firstHorse = ref([]);
 
-
-
 function randomHorseSpeed(horse) {
-    
-    const horseSpeed = setInterval(() => {
-        if(horse.speed < 1100)
-        {
-            horse.speed = horse.speed+(Math.floor(Math.random() * 10)*10);
-        }
-        else
-        {
-            firstHorse.value.push(horse.name);
-            clearInterval(horseSpeed);
-        }
-    },100)
+  const horseSpeed = setInterval(() => {
+    if (horse.speed < 1050) {
+      horse.speed = horse.speed + Math.floor(Math.random() * 100);
+    } else {
+      firstHorse.value.push(horse.id);
+      clearInterval(horseSpeed);
+    }
+  }, 100);
 }
 
-
-watch(props,() => {
-    if(props.gameStatusProp){
-        randomHorseSpeed(data.value[0]);
-        randomHorseSpeed(data.value[1]);
-        randomHorseSpeed(data.value[2]);
-        randomHorseSpeed(data.value[3]);
-        randomHorseSpeed(data.value[4]);
-        randomHorseSpeed(data.value[5]);
-    }
-})
-
-
+watch(props, () => {
+  if (props.gameStatusProp) {
+    randomHorseSpeed(data.value[0]);
+    randomHorseSpeed(data.value[1]);
+    randomHorseSpeed(data.value[2]);
+    randomHorseSpeed(data.value[3]);
+    randomHorseSpeed(data.value[4]);
+    randomHorseSpeed(data.value[5]);
+  }
+});
 </script>
 
 <template>
-    <div style="background-color:white; font-size: 30px;">
-    Kazanan at{{firstHorse[0]}}
+  <Result></Result>
+  <div style="background-color: white; font-size: 30px">
+    Kazanan {{ firstHorse[0] }} numaralı at.  
+  </div>
+  <div class="container" id="att">
+    <div class="at" v-bind:style="[{ left: data[0].speed + 'px' }]">
+      <img
+        src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif"
+        style="width: 120px"
+      />
     </div>
- <div class="container" id="att">
-    <div class="at" v-bind:style="[{left: data[0].speed+ 'px'}]" >
-       <img src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif" style="width:120px" />
+    <div class="at" v-bind:style="[{ left: data[1].speed + 'px' }]">
+      <img
+        src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif"
+        style="width: 120px"
+      />
     </div>
-    <div class="at" v-bind:style="[{left: data[1].speed + 'px'}]">
-        <img src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif" style="width:120px" />
+    <div class="at" v-bind:style="[{ left: data[2].speed + 'px' }]">
+      <img
+        src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif"
+        style="width: 120px"
+      />
     </div>
-    <div class="at" v-bind:style="[{left: data[2].speed + 'px'}]">
-        <img src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif" style="width:120px" />
+    <div class="at" v-bind:style="[{ left: data[3].speed + 'px' }]">
+      <img
+        src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif"
+        style="width: 120px"
+      />
     </div>
-    <div class="at" v-bind:style="[{left: data[3].speed + 'px'}]">
-        <img src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif" style="width:120px" />
+    <div class="at" v-bind:style="[{ left: data[4].speed + 'px' }]">
+      <img
+        src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif"
+        style="width: 120px"
+      />
     </div>
-    <div class="at" v-bind:style="[{left: data[4].speed + 'px'}]">
-        <img src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif" style="width:120px" />
+    <div class="at" v-bind:style="[{ left: data[5].speed + 'px' }]">
+      <img
+        src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif"
+        style="width: 120px"
+      />
     </div>
-    <div class="at" v-bind:style="[{left: data[5].speed + 'px'}]">
-        <img src="https://galeri14.uludagsozluk.com/760/gunun-gif-i_1299032.gif" style="width:120px" />
-    </div>
- </div>
-
-
+  </div>
 </template>
 
 <style scoped>
-.container{
-    background-image: url("../../../public/green.png");
-    background-repeat: no-repeat;
-    background-size: cover;
-    border: 1px solid black;
-    display: flex;
-    flex-direction: column;
-    margin: 50px;
+.container {
+  background-image: url("../../../public/green.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  margin: 50px;
 }
-.at{
-    position: relative;
-    color:white;
-    width: 25px;
-    animation-duration: 4s;
-
+.at {
+  position: relative;
+  color: white;
+  width: 25px;
+  animation-duration: 4s;
 }
 </style>
---
+++ final_project/src/components/result/Result.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+<template>
+    <div class="modal_container">
+        <div class="modal">
+            sonuclar
+        </div>
+        <button class="button"  >Tekrar başlat</button>
+    </div>
+</template>
+
+<style scoped>
+
+.modal_container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    height: auto;
+    flex-direction: column;
+    z-index: 99;
+}
+.modal{
+    background-color: orange;
+    width: 800px;
+    height: 500px;
+    margin: 10px;
+    padding: 10px;
+}
+.button{
+    width: 250px;
+    height: 50px;
+    border-radius: 15px;
+    margin: 10px;   
+    background-color: rgb(193, 248, 248);
+}
+.button:hover{
+    width: 250px;
+    height: 50px;
+    border-radius: 15px;
+    margin: 10px;
+    background-color: azure; 
+}
+</style>
